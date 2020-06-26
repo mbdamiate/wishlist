5ca8b30e80dc0450054e1e20fd06738f0c26dc26
Matheus B. Damiate <matheus.b.damiate@avanade.com>
2020-06-26 20:12:32 -0300

    Add user function 'update'

---
+++ src/api/routes/home.route.js
@@ -0,0 +1,9 @@
+
+const { Router } = require('express')
+const route = Router()
+
+const controller = require('../controller/home.controller')
+
+route.get('/', controller.welcome)
+
+module.exports = route
