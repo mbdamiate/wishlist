bb4cabd8bc95efbf0564170b8eee4521b07c8df2
Matheus B. Damiate <matheus.b.damiate@avanade.com>
2020-06-27 20:02:10 -0300

    Removed home 'module'

--- src/api/routes/home.route.js
+++
@@ -1,9 +0,0 @@
-
-const { Router } = require('express')
-const route = Router()
-
-const controller = require('../controller/home.controller')
-
-route.get('/', controller.welcome)
-
-module.exports = route
--- src/config/app.js
+++ src/config/app.js
@@ -8,7 +8,6 @@ const methodOverride = require('method-override')
 
 const app = express()
 
-const home = require('../api/routes/home.route')
 const user = require('../api/routes/user.route')
 
 app.use(bodyParser.json())
@@ -17,7 +16,6 @@ app.use(morgan('combined'))
 app.use(cors())
 app.use(methodOverride())
 
-app.use('/', home)
 app.use('/api/users', user)
 
 module.exports = app
